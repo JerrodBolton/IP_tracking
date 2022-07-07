6687993337f20fd825f208fbae819a36195bc97a
Jerrod <JerrodBolton2000@gmail.com>
2022-07-07 10:17:09 -0500

    frontend check

--- IP_tracking_backend/ip_locator_project/urls.py
+++ IP_tracking_backend/ip_locator_project/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('', admin.site.urls),
-     path('api-auth/', include('rest_framework.urls')), 
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('ip_locator_project')),
 
 ]
